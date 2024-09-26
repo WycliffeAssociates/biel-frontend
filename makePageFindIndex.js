ce054945b76537c50fa429c0a9c67a3792bcfd4d
Will Kelly <willkelly9617@gmail.com>
2024-09-26 15:57:32 -0500

    checkpoint4 working

--- makePageFindIndex.js
+++ makePageFindIndex.js
@@ -1,6 +1,6 @@
 import * as pagefind from "pagefind";
 import {getWpmlLanguages, getResourcePageSlugs} from "./src/data/wp";
-import getLanguagesWithContentForBiel from "./src/data/pubDataApi";
+import {getLanguagesWithContentForBiel} from "./src/data/pubDataApi";
 // Create a Pagefind search index to work with
 const {index} = await pagefind.createIndex();
 
@@ -10,58 +10,11 @@ await index.addDirectory({
   path: "dist",
 });
 // todo: remove translations.json with new pub data api
-const loc = process.env.TRANSLATIONS_JSON_ENDPOINT;
-const td = await fetch(loc);
-
-const translationsDataBlob = await td.blob();
-const translationsDataText = await translationsDataBlob.text();
-const translationsData = await JSON.parse(translationsDataText);
-
 const wpInstanceUrl = `${process.env.WORDPRESS_GQL_URL}`;
-const query = `query getTranslationsPage {
-  page(id: "translations", idType: URI) {
-    languageCode
-    title
-    translations {
-      languageCode
-      title
-      slug
-      uri
-    }
-  }
-}`;
-const langsRes = await fetch(wpInstanceUrl, {
-  method: "POST",
-  headers: {"Content-Type": "application/json"},
-  body: JSON.stringify({
-    query: query,
-  }),
-});
-const translationPages = await langsRes.json();
-// So we don't have to do anything extra for english and can just go through the translations array for single loop logic;
-translationPages.data.page.translations.push({
-  languageCode: "en",
-  title: "English",
-});
-
-for await (const lang of translationsData) {
-  for await (const translationPage of translationPages.data.page.translations) {
-    const baseUrl =
-      translationPage.languageCode === "en"
-        ? "translations"
-        : `${translationPage.languageCode}/${translationPage.slug}`;
-
-    const contents = lang.contents
-      .map((content) => {
-        return content.name;
-      })
-      .join("\n");
-    const theContent = `
-    ${lang.name}
-    ${contents}
-    `;
-
-    await index.addCustomRecord({
+const langs = await getWpmlLanguages({gqlUrl: wpInstanceUrl});
+// todo: resouce slugs and resource pages
+/* 
+  await index.addCustomRecord({
       url: `${baseUrl}?lang=${lang.code}`,
       content: `${theContent}`,
       language: translationPage.languageCode,
@@ -74,8 +27,9 @@ for await (const lang of translationsData) {
             : "10",
       },
     });
-  }
-}
+*/
+console.log({langs});
+
 // for dev
 await index.writeFiles({
   outputPath: "./src/pagefind",
