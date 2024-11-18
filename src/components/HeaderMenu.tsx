ef5e893d2a1f31b709aa1f623a62e84658c68285
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2024-11-18 14:08:08 -0600

    try manually passing in frozen lockfile in ci job

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -17,8 +17,8 @@ type HeaderMenuProps = {
 };
 export function HeaderMenu(props: HeaderMenuProps) {
   const [activeIdx, setActiveIdx] = createSignal<number | null>();
-  const isBig = createMediaQuery("(min-width: 900px)", true);
-  const canHover = createMediaQuery("(hover: hover)", true);
+  const isBig = createMediaQuery("(min-width: 900px)", false);
+  const canHover = createMediaQuery("(hover: hover)", false);
   function shapeLink(item: MenuItem) {
     const link = item.attached_post?.uri || item.url;
     if (isAbsoluteUrl(link)) return link;
