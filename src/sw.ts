440ac6d2772db6ea39714ea1d16aecbc78c7059c
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2025-05-27 16:48:37 -0500

    update service worker caching for index

--- src/sw.ts
+++ src/sw.ts
@@ -5,7 +5,7 @@ import {clientsClaim} from "workbox-core";
 import {ExpirationPlugin} from "workbox-expiration";
 import {cleanupOutdatedCaches, precacheAndRoute} from "workbox-precaching";
 import {registerRoute} from "workbox-routing";
-import {CacheFirst} from "workbox-strategies";
+import {CacheFirst, NetworkFirst} from "workbox-strategies";
 import {constants} from "./lib/constants";
 import {
   bielExternalCacheName,
@@ -13,7 +13,7 @@ import {
   bielStaticCacheName,
   fetchExternalUsfmAndCache,
 } from "./lib/web";
-import {Strategy, type StrategyHandler} from "workbox-strategies";
+import {} from "workbox-strategies";
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -26,42 +26,42 @@ const manifest = self.__WB_MANIFEST;
 console.log({manifest});
 import.meta.env.PROD && precacheAndRoute(manifest);
 
-class CacheBustQueryParamRaceStrategy extends Strategy {
-  _handle(request: Request, handler: StrategyHandler): Promise<Response> {
-    const urlCopy = new URL(request.url);
-    const searchParams = new URLSearchParams(urlCopy.searchParams);
-    searchParams.append("cache-bust", "1");
-    urlCopy.search = searchParams.toString();
-    const requestToMake = new Request(urlCopy.toString(), request);
-    // match w/o search params, but fetch freshest from network with cache bust always:
-    const fetchAndCachePutDone = handler.fetchAndCachePut(requestToMake);
-    const cacheMatchDone = handler.cacheMatch(request);
+// class CacheBustQueryParamRaceStrategy extends Strategy {
+//   _handle(request: Request, handler: StrategyHandler): Promise<Response> {
+//     const urlCopy = new URL(request.url);
+//     const searchParams = new URLSearchParams(urlCopy.searchParams);
+//     searchParams.append("cache-bust", "1");
+//     urlCopy.search = searchParams.toString();
+//     const requestToMake = new Request(urlCopy.toString(), request);
+//     // match w/o search params, but fetch freshest from network with cache bust always:
+//     const fetchAndCachePutDone = handler.fetchAndCachePut(requestToMake);
+//     const cacheMatchDone = handler.cacheMatch(request);
 
-    return new Promise((resolve, reject) => {
-      fetchAndCachePutDone.then(resolve);
-      cacheMatchDone.then(
-        (response: Response | undefined) => response && resolve(response)
-      );
+//     return new Promise((resolve, reject) => {
+//       fetchAndCachePutDone.then(resolve);
+//       cacheMatchDone.then(
+//         (response: Response | undefined) => response && resolve(response)
+//       );
 
-      // Reject if both network and cache error or find no response.
-      Promise.allSettled([fetchAndCachePutDone, cacheMatchDone]).then(
-        (results) => {
-          const [
-            fetchAndCachePutResult,
-            cacheMatchResult,
-          ]: PromiseSettledResult<Response | undefined>[] = results;
-          if (
-            fetchAndCachePutResult.status === "rejected" &&
-            // @ts-ignore
-            !cacheMatchResult?.value
-          ) {
-            reject(fetchAndCachePutResult.reason);
-          }
-        }
-      );
-    });
-  }
-}
+//       // Reject if both network and cache error or find no response.
+//       Promise.allSettled([fetchAndCachePutDone, cacheMatchDone]).then(
+//         (results) => {
+//           const [
+//             fetchAndCachePutResult,
+//             cacheMatchResult,
+//           ]: PromiseSettledResult<Response | undefined>[] = results;
+//           if (
+//             fetchAndCachePutResult.status === "rejected" &&
+//             // @ts-ignore
+//             !cacheMatchResult?.value
+//           ) {
+//             reject(fetchAndCachePutResult.reason);
+//           }
+//         }
+//       );
+//     });
+//   }
+// }
 
 registerRoute(
   ({request}) => {
@@ -183,8 +183,8 @@ registerRoute(
       !url.searchParams.get("cache-bust")
     );
   },
-  // this is a SWR like strategy, but we also while the revalidting to do a cache-bust query param fetch.
-  new CacheBustQueryParamRaceStrategy({
+  // Having caching issues with this route
+  new NetworkFirst({
     cacheName: bielStaticCacheName,
     plugins: [
       new ExpirationPlugin({
